dd69cdf2b922964488cd1025534cc5013e84c6c7
Davis Sneed <dlsneed1298@gmail.com>
2025-02-12 22:39:55 -0800

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Author: Davis Sneed
+Created 2/12/2025
+description: This is the main file for the project. Lets see where this goes ig...
+"""
+
+def main():
+    return
+
+if __name__ == "__main__":
+    main()
